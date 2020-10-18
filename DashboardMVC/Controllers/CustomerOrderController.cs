60962496ff6fb412b316144baa9cbba83c1c4c23
Gabriel Modesto <gabrielrmodesto5@gmail.com>
2020-10-18 18:29:18 -0300

    Lista de customers order

---
+++ DashboardMVC/Controllers/CustomerOrderController.cs
@@ -0,0 +1,43 @@
+﻿using DashboardMVC.DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DashboardMVC.Controllers
+{
+    public class CustomerOrderController : Controller
+    {
+        // GET: CustomerOrder
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult CustomerList()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult GetCustomers()
+        {
+            using (DashboardContext _context = new DashboardContext())
+            {
+                var customerList = _context.CustomerSet.Select(c => new
+                {
+                    c.ID,
+                    c.CustomerName,
+                    c.CustomerEmail,
+                    c.CustomerPhone,
+                    c.CustomerCountry,
+                    c.CustomerImage
+                }).ToList();
+
+                return Json(new { data = customerList }, JsonRequestBehavior.AllowGet);
+            }
+        }
+    }
+}
